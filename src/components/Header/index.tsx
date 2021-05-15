e6ff11e919fbe51651e9e1c9b6efd132d8ebbaf1
Matheus <matheuseufrasio2@gmail.com>
2021-05-14 23:55:35 -0300

    Header iniciado

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.scss';
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <p>Larissa Silberman</p>
+    </header>
+  )
+}
